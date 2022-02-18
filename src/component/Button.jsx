84b0a6f9635da2b76979bd6533920f9720576d47
Truth <henrysempire111gmail.com>
2022-02-18 05:31:11 -0800

    made some ui change

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = () => {
-  return <button className="rounded-md py-2 px-8 lg:mt-8 mt-4 text-sm font-semibold text-white flex mx-auto " style={{backgroundColor:'#666AF6'}}>REGISTER</button>
+  return <button className="rounded-md py-2 px-8 lg:mt-8 mt-1 text-sm font-semibold text-white flex mx-auto " style={{backgroundColor:'#666AF6'}}>REGISTER</button>
 }
 
 export default Button
--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -12,7 +12,7 @@ const Auth = () => {
     return <div className="flex flex-col mt-36   md:flex-row md:mx-0 lg:px-12">
         <section className="bg-white  basis-1/2">
         <div className="shadow-lg w-72 mx-auto md:mx-12 lg:mx-auto lg:w-96 lg:mx-0 h-80 lg:h-96  lg:mt-12 ">
-            <h1 className="ml-5 font-semibold py-2 lg:py-6">REGISTER FOR AN ONLINE CLASS</h1>
+            <h1 className="ml-5 lg:text-lg font-semibold py-2 lg:py-6">REGISTER FOR AN ONLINE CLASS</h1>
             <Input onChange={(e) => setUsername(e)} label={'USERNAME'}/>
             <Input onChange={(e) => setEmail(e)} label={'EMAIL'}/>
             <Input onChange={(e) => setPassword(e)} label={'PASSWORD'}/>

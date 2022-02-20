6cdb43bb66b4843dd8516eef51581dfaa9309e62
Truth <henrysempire111gmail.com>
2022-02-19 23:55:23 -0800

    merging request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Auth from './pages/auth'
+// import Auth from './pages/auth'
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  // return <Auth></Auth>
   return <Dashboard/>
   
 }
-
 export default App;

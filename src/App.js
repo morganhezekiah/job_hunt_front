cfda47993b1f2d6f25d990f7f110c28564e9a344
cyba <morganhezekiah59@gmail.com>
2022-03-03 00:02:00 +0100

    frontent changes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import { ToastContainer } from "react-toastify";
 import ForgotPassword from "./app/pages/Auth/ForgotPassword";
 import Dashboard from "./app/pages/Dashboard/Dashboard";
 import EmailActivation from "./app/pages/Auth/EmailActivation";
-<<<<<<< HEAD
 import Login from "./app/pages/Auth/Login";
 import Register from "./app/pages/Auth/Register";
 import Logout from "./app/pages/Auth/Logout";
 import LandingPage from "./app/pages/LandingPage/LandingPage";
-=======
-import LandingPage  from "./app/pages/LandingPage";
-import Login from './app/pages/Auth/Login'
->>>>>>> 794576fed6636589861a9ff3346a8e0efa70e69a
 
 
 const App = () => {
@@ -32,7 +27,6 @@ const App = () => {
       />
       <Provider store={ store }>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<LandingPage />} />
         <Route path="/users/login" element={<Login />} />
         <Route path="/users/register" element={<Register />} />
@@ -40,14 +34,6 @@ const App = () => {
         <Route path="/users/logout" element={<Logout />} />
         <Route path="/users/forgot-password" element={<ForgotPassword />} />
         <Route path="/users/user-email-activation/:randomString1/:token/:randomString2" element={<EmailActivation />} />
-=======
-         <Route path="/" element={<Login />} />
-        {/* <Route path="/users/login" element={<Login />} /> */}
-        <Route path="/users/register" element={<Dashboard/>} />  
-        <Route path="/users/dashboard" element={<LandingPage/>} />
-        {/* <Route path="/users/forgot-password" element={<ForgotPassword />} />
-        <Route path="/users/user-email-activation/:randomString1/:token/:randomString2" element={<EmailActivation />} /> */}
->>>>>>> 794576fed6636589861a9ff3346a8e0efa70e69a
       </Routes>
       </Provider>
     </Router>
